1b4a610787af9242407b197bbf2b74206f9de597
DianaNilsson <diana.sofie.nilsson@gmail.com>
2019-11-06 09:45:20 +0100

    Loopat in mätstationerna i formuläret samt lagt till en * i CSS-fil relates # 9

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
-var respons;
-
+var res;
+var selectMeasuresite = document.getElementById("select-measuresite");
+var optionMeasuresites;
 
 fetch('http://data.goteborg.se/RiverService/v1.1/MeasureSites/f7a16e1a-1f8f-44f7-9230-54bdc02ac2ba?format=json&limit=10')
 .then(function(response) {
 return response.json();
 })
 .then(function(myRes) {
-    respons = myRes;
+    res = myRes;
     render();
 });
 
 function render() {
-    console.log(JSON.stringify(respons));
-    /* Add DOM and Events here */
+    console.log(JSON.stringify(res));
+    for (var i = 0; i < res.length; i++ ){
+        optionMeasuresites = document.createElement("option");
+        optionMeasuresites.innerHTML = res[i].Code;
+        selectMeasuresite.appendChild(optionMeasuresites);
+    }
 }
